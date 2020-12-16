6194f800088c403de9c0a4aca8ed4deae981ca8f
rodrigorhas <rodrigorhas@gmail.com>
2020-12-16 01:58:31 -0300

    - fix sequelize version - cleanup config file

--- src/config/config.js
+++ src/config/config.js
@@ -1,29 +1,33 @@
-import tedious from 'tedious'
-
 export default {
-  development: {
-    username: "sa",
-    password: "R0dr1g0rh45!@#$",
-    database: "pokemon",
-    host: "127.0.0.1",
-    dialect: "mysql",
-    dialectModule: tedious
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mssql",
-    dialectModule: tedious
-  },
-  production: {
-    username: "DesafioAdmin",
-    password: "Picachu123",
-    database: "Desafio-jz",
-    schema: "RODRIGO",
-    host: "127.0.0.1",
-    dialect: "mssql",
-    dialectModule: tedious
-  }
+    development: {
+        username: "sa",
+        password: "R0dr1g0rh45!@#$",
+        database: "pokemon",
+        host: "127.0.0.1",
+        dialect: "mysql",
+        dialectOptions: {
+            instanceName: 'master'
+        },
+    },
+    test: {
+        username: "root",
+        password: null,
+        database: "database_test",
+        host: "127.0.0.1",
+        dialect: "mssql",
+        dialectOptions: {
+            instanceName: 'master'
+        },
+    },
+    production: {
+        username: "DesafioAdmin",
+        password: "Picachu123",
+        database: "Desafio-jz",
+        schema: "RODRIGO",
+        host: "127.0.0.1",
+        dialect: "mssql",
+        dialectOptions: {
+            instanceName: 'master'
+        },
+    }
 }
